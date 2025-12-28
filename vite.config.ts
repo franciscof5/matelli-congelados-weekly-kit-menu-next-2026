d6f8d6a47725d4ce172ca81429bc8b4fcd9ec621
Francisco Matelli <fmatelli@gmail.com>
2025-12-28 01:19:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: '/matelli-congelados-weekly-kit-menu-next-2026/',
     const env = loadEnv(mode, '.', '');
     return {
       server: {
