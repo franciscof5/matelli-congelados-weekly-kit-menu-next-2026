a2f9518e61bb046dcb8ddb64d30aff8a633fb1e7
Francisco Matelli <francisco@f5sites.com>
2025-12-28 01:32:56 -0300

    vite without importmap

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/matelli-congelados-weekly-kit-menu-next-2026/',
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/matelli-congelados-weekly-kit-menu-next-2026/',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -13,7 +13,8 @@ export default defineConfig(({ mode }) => {
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.NODE_ENV': JSON.stringify(mode)
       },
       resolve: {
         alias: {
